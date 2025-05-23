9b7ab58e6a1e19f64c4cf398c909c30267012de6
sazidc <sazidc01@gmail.com>
2025-05-23 19:54:23 +0600

    Day-18 Turtle draw shapes good soln

--- pyhdoc/day_018/day_18_start.py
+++ pyhdoc/day_018/day_18_start.py
@@ -1,10 +1,12 @@
 ## Using turtle module to make a square
 
 from turtle import Turtle, exitonclick
+import random
 
 tim = Turtle()
 
-tim.color("cadetblue")
+# tim.color("cadetblue")
+
 # for _ in range(4):
 #     tim.forward(100)
 #     tim.right(90)
@@ -24,51 +26,64 @@ tim.color("cadetblue")
 
 # # draw different shapes like triangle, square, pentagon, hexagon, etc
 
-for _ in range(3):
-    tim.forward(100)
-    tim.right(120)
+# for _ in range(3):
+#     tim.forward(100)
+#     tim.right(120)
 
-tim.color("red")
+# tim.color("red")
 
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
 
-tim.color("green")
+# tim.color("green")
 
-for _ in range(5):
-    tim.forward(100)
-    tim.right(72)
+# for _ in range(5):
+#     tim.forward(100)
+#     tim.right(72)
 
-tim.color("blue")
+# tim.color("blue")
 
-for _ in range(6):
-    tim.forward(100)
-    tim.right(60)
+# for _ in range(6):
+#     tim.forward(100)
+#     tim.right(60)
 
-tim.color("brown")
+# tim.color("brown")
 
-for _ in range(7):
-    tim.forward(100)
-    tim.right(51.43)
+# for _ in range(7):
+#     tim.forward(100)
+#     tim.right(51.43)
 
-tim.color("orange")
+# tim.color("orange")
 
-for _ in range(8):
-    tim.forward(100)
-    tim.right(45)
+# for _ in range(8):
+#     tim.forward(100)
+#     tim.right(45)
 
-tim.color("violet")
+# tim.color("violet")
 
-for _ in range(9):
-    tim.forward(100)
-    tim.right(40)
+# for _ in range(9):
+#     tim.forward(100)
+#     tim.right(40)
 
-tim.color("black")
+# tim.color("black")
 
-for _ in range(10):
-    tim.forward(100)
-    tim.right(36)
+# for _ in range(10):
+#     tim.forward(100)
+#     tim.right(36)
 
-exitonclick()
+# exitonclick()
 
+# # Draw shapes easier solution
+
+colors = ["cadetblue", "red", "green", "blue", "brown", "orange", "violet", "black"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
