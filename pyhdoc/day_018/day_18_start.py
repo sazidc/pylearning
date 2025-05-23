62dfd03daf57b6ad6803f9a9c199e0b479d97758
sazidc <sazidc01@gmail.com>
2025-05-23 19:42:23 +0600

    Day-18 Turtle draw dashed lines

--- pyhdoc/day_018/day_18_start.py
+++ pyhdoc/day_018/day_18_start.py
@@ -1,12 +1,26 @@
-# Using turtle module to make a square
+## Using turtle module to make a square
 
-from turtle import Turtle
+from turtle import Turtle, exitonclick
 
 tim = Turtle()
 
-tim.color("coral")
-for _ in range(4):
-    tim.forward(100)
-    tim.right(90)
+tim.color("cadetblue")
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
 
-tim.exitonclick()
+# exitonclick()
+
+
+# draw dashed lines 50 times
+
+for _ in range(20):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+exitonclick()
+
+
+
