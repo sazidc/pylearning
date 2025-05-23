b61668dfb30fd38cbaf868e68fa855c0770a06fd
sazidc <sazidc01@gmail.com>
2025-05-23 22:10:40 +0600

    Day-18 Turtle draw spirograph

--- pyhdoc/day_018/day_18_start.py
+++ pyhdoc/day_018/day_18_start.py
@@ -110,17 +110,31 @@ def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    random_color = (r, g, b)
-    return random_color
+    color = (r, g, b)
+    return color
 
-direction = [0, 90, 180, 270]
-tim.pensize(15)
+# direction = [0, 90, 180, 270]
+# tim.pensize(15)
+# tim.speed("fastest")
+
+
+# for _ in range(200):
+#     tim.color(random_color())
+#     tim.setheading(random.choice(direction))
+#     tim.forward(30)
+
+# t.exitonclick()
+
+
+# # create a spirograph
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
 tim.speed("fastest")
-
-
-for _ in range(200):
-    tim.color(random_color())
-    tim.setheading(random.choice(direction))
-    tim.forward(30)
+draw_spirograph(6)
 
 t.exitonclick()
