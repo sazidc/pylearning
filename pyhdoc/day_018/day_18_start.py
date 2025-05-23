5d364580fd9a3982a53f3ff8d191f44d72c1c024
sazidc <sazidc01@gmail.com>
2025-05-23 21:42:53 +0600

    Day-18 Turtle draw random art

--- pyhdoc/day_018/day_18_start.py
+++ pyhdoc/day_018/day_18_start.py
@@ -76,14 +76,28 @@ tim = Turtle()
 
 # # Draw shapes easier solution
 
-colors = ["cadetblue", "red", "green", "blue", "brown", "orange", "violet", "black"]
+# colors = ["cadetblue", "red", "green", "blue", "brown", "orange", "violet", "black"]
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
 
-for shape_side_n in range(3, 11):
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colors))
+#     draw_shape(shape_side_n)
+
+# # draw random walk
+
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+direction = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
     tim.color(random.choice(colors))
-    draw_shape(shape_side_n)
+    tim.setheading(random.choice(direction))
+    tim.forward(30)
+
+exitonclick()
