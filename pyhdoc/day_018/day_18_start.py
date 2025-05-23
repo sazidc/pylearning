20d7828a0e6aceaadec4c93d34325b9fd57af5b9
sazidc <sazidc01@gmail.com>
2025-05-23 19:16:04 +0600

    Day-18 Turtle module square shape

---
+++ pyhdoc/day_018/day_18_start.py
@@ -0,0 +1,15 @@
+# Using turtle module to make a square
+
+import turtle as timmy_the_turtle
+
+timmy_the_turtle.color("coral")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+
+timmy_the_turtle.exitonclick()
