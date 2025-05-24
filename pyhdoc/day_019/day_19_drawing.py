5f744d837a2a1095158b0b070c877ea842447411
sazidc <sazidc01@gmail.com>
2025-05-24 23:36:36 +0600

    Day-19 drawing with keystroke

---
+++ pyhdoc/day_019/day_19_drawing.py
@@ -0,0 +1,35 @@
+# Drawing with turtle using keystroke
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+screen = Screen()
+
+def move_forwards():
+    timmy.forward(10)
+
+def move_backwards():
+    timmy.backward(10)
+
+def turn_left():
+    timmy.left(10)
+
+def turn_right():  
+    timmy.right(10)
+
+def clear():
+    timmy.clear()
+    timmy.penup()
+    timmy.home()
+    timmy.pendown()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear)
+screen.exitonclick()
+
+
+
