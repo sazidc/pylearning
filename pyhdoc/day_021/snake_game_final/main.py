4de16ac2d55d65fcca760daa0acc9d0b578a40ab
sazidc <sazidc01@gmail.com>
2025-05-27 14:55:15 +0600

    Day-21 Snake Game full fixed and restart game logic added

--- pyhdoc/day_021/snake_game_final/main.py
+++ pyhdoc/day_021/snake_game_final/main.py
@@ -7,45 +7,55 @@ from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My snake game")
-screen.tracer(0)
 
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+def play_game():
+    screen.clearscreen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My snake game")
+    screen.tracer(0)
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
-    # Detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-    
-    # Detect collision with wall
-    boundary = 280
-    segment_size = 20
-    if (snake.head.xcor() > boundary + segment_size/2 or snake.head.xcor() < -boundary - segment_size/2 or snake.head.ycor() > boundary + segment_size/2 or snake.head.ycor() < -boundary - segment_size/2):
-        game_is_on = False
-        scoreboard.game_over()
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
-    # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
+        # Detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
+        
+        # Detect collision with wall
+        segment_size = 20
+        boundary_x = screen.window_width()//2 - segment_size
+        boundary_y = screen.window_height()//2 - segment_size
+        if (snake.head.xcor() > boundary_x + segment_size/2 or snake.head.xcor() < -boundary_x - segment_size/2 or snake.head.ycor() > boundary_y + segment_size/2 or snake.head.ycor() < -boundary_y - segment_size/2):
             game_is_on = False
             scoreboard.game_over()
 
+        # Detect collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
 
-screen.exitonclick()
+    # Ask player if they want to play again            
+    answer = screen.textinput("Play Again?", "Do you want to play again? (yes/no)")
+    if answer is not None and answer.lower() in ["yes", "y"]:
+        play_game() 
+    else:
+        screen.bye()
+
+play_game()
