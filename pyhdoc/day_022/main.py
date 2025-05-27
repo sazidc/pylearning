ad2d251e46e1e17a52073798844ebb9689caa0ec
sazidc <sazidc01@gmail.com>
2025-05-27 21:38:33 +0600

    Day-22 Pong game created ball

--- pyhdoc/day_022/main.py
+++ pyhdoc/day_022/main.py
@@ -2,7 +2,8 @@
 
 from turtle import Screen
 from paddle import Paddle
-
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -10,8 +11,9 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-r_paddle = Paddle((370, 0))
-l_paddle = Paddle((-380, 0))
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-360, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -19,7 +21,13 @@ screen.onkey(r_paddle.go_down, "Down")
 screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
 
-screen.update()
+    
+
 
 screen.exitonclick()
