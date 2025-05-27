4003c443b055598ff70e2bc0b8d338271351fd63
sazidc <sazidc01@gmail.com>
2025-05-27 23:07:18 +0600

    Day-22 Pong added ball bounce on paddles

--- pyhdoc/day_022/main.py
+++ pyhdoc/day_022/main.py
@@ -29,8 +29,11 @@ while game_is_on:
 
     # Detect collision with wall
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
-    
+        ball.bounce_y()
+
+    # Detect collision with paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -330:
+        ball.bounce_x()
 
 
 screen.exitonclick()
