bce61be8a088f0dc5e20b03d35ff780efc03e8a8
sazidc <sazidc01@gmail.com>
2025-05-27 22:59:28 +0600

    Day-22 Pong added ball bounce on wall

--- pyhdoc/day_022/ball.py
+++ pyhdoc/day_022/ball.py
@@ -18,3 +18,6 @@ class Ball(Turtle):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
