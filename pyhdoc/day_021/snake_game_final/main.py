9a44c88526b9432ea71cafe1915dac32e8dea268
sazidc <sazidc01@gmail.com>
2025-05-27 12:08:30 +0600

    Day-21 Snake Game added collision with wall

--- pyhdoc/day_021/snake_game_final/main.py
+++ pyhdoc/day_021/snake_game_final/main.py
@@ -3,6 +3,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -29,8 +31,12 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
-        
-
+        scoreboard.increase_score()
+    
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -300 or snake.head.ycor() > 275 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
     
 
 
