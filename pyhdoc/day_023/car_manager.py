ba98455eb3fc68137f59d9c0de0a045c0a3f67c1
sazidc <sazidc01@gmail.com>
2025-05-28 20:54:52 +0600

    Day-23 Turtle Crossing car_manager created

---
+++ pyhdoc/day_023/car_manager.py
@@ -0,0 +1,34 @@
+# CarManager Class for the Turtle Crossing game
+
+import random
+from turtle import Turtle
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 5
+
+class CarManager:
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_car(self):
+        # Increase spawn frequency as speed increases
+        spawn_chance = max(3, 10 - self.car_speed)  
+        if random.randint(1, spawn_chance) == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+        # Remove cars off screen
+        self.all_cars = [car for car in self.all_cars if car.xcor() > -320]
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
