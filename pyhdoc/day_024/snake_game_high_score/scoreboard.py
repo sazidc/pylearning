cb93b9621e10b9eb58d6dabb0d479a538b5c5e80
sazidc <sazidc01@gmail.com>
2025-05-30 11:47:01 +0600

    Day-24 Snake game high score keeping feature

--- pyhdoc/day_024/snake_game_high_score/scoreboard.py
+++ pyhdoc/day_024/snake_game_high_score/scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("high_score.txt") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -22,6 +23,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("high_score.txt", mode="w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
