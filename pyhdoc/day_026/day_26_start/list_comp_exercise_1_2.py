57fd5c1c80adf0e1ef1c507d0f66748da3e72595
sazidc <sazidc01@gmail.com>
2025-06-01 12:21:14 +0600

    Day-26 Exercise 1 & 2 list comprehension

---
+++ pyhdoc/day_026/day_26_start/list_comp_exercise_1_2.py
@@ -0,0 +1,11 @@
+# Exercise 1
+
+numbers = [1, 1, 4, 5, 8, 13, 21, 34, 55]
+
+squared_numbers = [n**2 for n in numbers]
+print(squared_numbers)
+
+# Exercise 2
+
+even_numbers = [n for n in numbers if n%2 == 0]
+print(even_numbers)
