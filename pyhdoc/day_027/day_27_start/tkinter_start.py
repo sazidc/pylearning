300e832d66bd194357ab339d65da3b364e3ff3c9
sazidc <sazidc01@gmail.com>
2025-06-02 23:19:12 +0600

    Day-27 tkinter layout config familiarization

--- pyhdoc/day_027/day_27_start/tkinter_start.py
+++ pyhdoc/day_027/day_27_start/tkinter_start.py
@@ -9,23 +9,28 @@ def button_clicked():
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
 
 # Label
 
-my_label = Label(text="This is a label", font=("Arial", 24, "bold"))
-my_label.pack()
-
+my_label = Label(text="This is a label", font=("Arial", 16, "bold"))
+my_label.grid(column=0, row=0)
 my_label.config(text="I AM SPARTA")
+my_label.config(padx=50, pady=50)
 
 # Button
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+new_button = Button(text="new button")
+new_button.grid(column=2, row=0)
 
 # Entry
 
 input = Entry(width=10)
-input.pack()
+print(input.get())
+input.grid(column=3, row=2)
 
 
 
