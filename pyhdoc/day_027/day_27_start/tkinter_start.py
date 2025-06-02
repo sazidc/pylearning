4eea2ab50743d41fdaf49379a9a700cde1ce29cc
sazidc <sazidc01@gmail.com>
2025-06-02 23:07:09 +0600

    Day-27 tkinter different widgets

--- pyhdoc/day_027/day_27_start/tkinter_start.py
+++ pyhdoc/day_027/day_27_start/tkinter_start.py
@@ -1,15 +1,31 @@
-import tkinter
+# # Tkinter - Graphical User Interface
 
-window = tkinter.Tk()
+from tkinter import Button, Label, Tk, Entry
+
+def button_clicked():
+    print("I got clicked")
+    my_label.config(text = input.get())
+
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="This is a label", font=("Arial", 24, "bold"))
+# Label
+
+my_label = Label(text="This is a label", font=("Arial", 24, "bold"))
 my_label.pack()
 
+my_label.config(text="I AM SPARTA")
 
+# Button
 
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
+# Entry
+
+input = Entry(width=10)
+input.pack()
 
 
 
