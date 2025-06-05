f70d9988a5cb5807a7793c2e0afb149bc5fd9e46
sazidc <sazidc01@gmail.com>
2025-06-05 11:14:28 +0600

    Day-28 Pomodoro timer completed checkmark and reset added

--- pyhdoc/day_028/pomodoro_timer/main.py
+++ pyhdoc/day_028/pomodoro_timer/main.py
@@ -8,19 +8,47 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 0.25
+SHORT_BREAK_MIN = 0.15
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    global timer
+    if timer is not None:
+        window.after_cancel(timer)
+        timer = None
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer", fg=GREEN)
+    check_mark.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    countdown(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
-def countdown(count):
+def count_down(count):
     """starts the countdown of timer"""
     count_min = math.floor(count / 60)
     count_sec = count % 60
@@ -29,7 +57,16 @@ def countdown(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, countdown, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        check_mark.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
@@ -45,13 +82,13 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(103, 130, text= "00:00", fill="white", font=(FONT_NAME, 30, "bold"))
 canvas.grid(column=1, row=1)
 
-start_button = tk.Button(text="Start", padx=10, pady=5, highlightthickness=0, command=start_timer)
+start_button = tk.Button(text="Start", padx=5, pady=2, highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = tk.Button(text="Reset", padx=10, pady=5, highlightthickness=0)
+reset_button = tk.Button(text="Reset", padx=5, pady=2, highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_mark = tk.Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
+check_mark = tk.Label( fg=GREEN, bg=YELLOW, font=(FONT_NAME, 16))
 check_mark.grid(column=1, row=3)
 
 
