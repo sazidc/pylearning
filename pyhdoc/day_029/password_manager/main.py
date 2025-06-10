a5453d2622b3970e7c956d65b6b287e79f2aa68b
sazidc <sazidc01@gmail.com>
2025-06-10 14:40:21 +0600

    Day-29 Password Manager Project logo set

---
+++ pyhdoc/day_029/password_manager/main.py
@@ -0,0 +1,21 @@
+# # Password Manager Project
+
+import tkinter as tk
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = tk.Canvas(width=200, height=200)
+logo_img = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.pack()
+
+
+window.mainloop()
